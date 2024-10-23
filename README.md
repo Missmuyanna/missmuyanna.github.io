9c6e4ed763296594758d7701d6654a28bb2fd104
Missjuly99 <fabiannajulio@gmail.com>
2024-10-23 16:32:42 -0500

    Inicializando nuevo repositorio

---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# missmuyanna.github.io
+PORTAFOLIO
+=======
+# missmuyanna.github.io-
+Pagina web mia 
+>>>>>>> d715231 (Primer commit)
---
+++ src/App.js
@@ -0,0 +1,45 @@
+// Importamos el archivo de estilos styles.css para aplicarle diseño a los componentes HTML.
+import './App.css';
+// Importamos el logo de React. Este archivo "logo.svg" viene por defecto al crear un proyecto de React.
+import logo from './logo.svg';
+// importar hashrouter 
+import { HashRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
+
+// Definimos el componente principal de la aplicación, llamado "App".
+// En React, los componentes son funciones o clases que devuelven código JSX, que es una mezcla de HTML y JavaScript.
+function App() {
+  // Aquí retornamos el HTML que se va a renderizar en la página.
+  return (
+    // Creamos un div contenedor principal.
+    <div style={{ 
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/images/gatos.jpg'})`, 
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      height: '100vh',
+      margin: 0
+    }}>
+
+      {/* Footer o pie de página */}
+      <footer>
+        {/* Información de contacto */}
+        <p>Contacto: fabiannajulio@gmail.com</p>
+      </footer>
+    </div>
+  );
+}
+
+// Exportamos el componente "App" para que pueda ser utilizado en otros archivos de la aplicación.
+// Es necesario exportarlo para que React pueda renderizar este componente en la aplicación.
+export default App;
