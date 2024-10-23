5717712f1d05b657652f0dfb8b050b0d95290cfb
Missjuly99 <fabiannajulio@gmail.com>
2024-10-22 22:06:15 -0500

    imagen de gatito

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,4 @@
 // Importamos el archivo de estilos styles.css para aplicarle diseño a los componentes HTML.
-import './styles.css';
-// Importamos el archivo App.css, que es otro archivo de estilos específico de React (esto también se genera por defecto).
 import './App.css';
 // Importamos el logo de React. Este archivo "logo.svg" viene por defecto al crear un proyecto de React.
 import logo from './logo.svg';
@@ -11,22 +9,14 @@ function App() {
   // Aquí retornamos el HTML que se va a renderizar en la página.
   return (
     // Creamos un div contenedor principal.
-    <div>
-
-      {/* Header de la página */}
-      <header>
-        {/* Título del portafolio */}
-        <h1>Bienvenido a mi portafolio</h1>
-
-        {/* Un párrafo con una breve descripción */}
-        <p>Aquí están algunos de mis proyectos:</p>
-
-        {/* Lista no ordenada que contiene los enlaces a tus proyectos */}
-        <ul>
-          <li><a href="#">Proyecto 1</a></li> 
-          <li><a href="#">Proyecto 2</a></li>
-        </ul>
-      </header>
+    <div style={{ 
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/images/gatos.jpg'})`, 
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      height: '100vh',
+      margin: 0
+    }}>
 
       {/* Footer o pie de página */}
       <footer>
